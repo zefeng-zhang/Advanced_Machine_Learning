d5184e6e06de4f021d28b3e1f25d68a6cd927491
zefeng-zhang <zzhang107@dons.usfca.edu>
2017-06-25 17:07:53 -0700

    add comments

--- naive_bayes_classifier/code.py
+++ naive_bayes_classifier/code.py
@@ -8,6 +8,9 @@ import numpy as np
 import pandas as pd
 
 def parse_data(filename):
+    """
+    return training and testing data sets
+    """
     df = pd.read_csv(filename, header=None)
     train, test = train_test_split(df, test_size=1.0/4.0)
     x_train = train.iloc[:, :-1]
@@ -17,6 +20,9 @@ def parse_data(filename):
     return train, test, x_train, y_train, x_test, y_test
 
 def get_summaries(df):
+    """
+    For each class/category, return mean and std of all features
+    """
     categories = df.iloc[:, -1].unique()
     summaries = defaultdict(list)
     for cat in categories:
@@ -29,10 +35,16 @@ def get_summaries(df):
     return summaries
 
 def gaussian_probability(x, mean, std):
+    """
+    Given mean and std, return the probability of getting x
+    """
     exponent = np.exp(-(x - mean)**2.0 / 2 / std**2.0)
     return 1.0/ np.sqrt(2.0*np.pi) / std * exponent
 
 def row_label(summaries, row):
+    """
+    Given a new observation, return the predicted label
+    """
     probability = {}
     for class_value, class_summary in summaries.iteritems():
         probability[class_value] = 1
@@ -42,13 +54,20 @@ def row_label(summaries, row):
     return max(probability, key=probability.get)
 
 def prediction(summaries, test_x):
+    """
+    Given a testing data set, return a list of predicted labels
+    """
     test_x['y'] = test_x.apply(lambda x: row_label(summaries, x), axis=1)
     return test_x['y']
 
 def report(y, y_pred):
+    """
+    Return prediction accuracy
+    """
     print "\naccuracy = {}".format( 1.0 * np.sum(np.array(y) == np.array(y_pred)) / len(y) )
 
-train, test, x_train, y_train, x_test, y_test = parse_data("pima-indians-diabetes.data.txt")
-summaries = get_summaries(train)
-y_pred = prediction(summaries, x_test)
-report(y_test, y_pred)
+if _name_ == "__main__":
+    train, test, x_train, y_train, x_test, y_test = parse_data("pima-indians-diabetes.data.txt")
+    summaries = get_summaries(train)
+    y_pred = prediction(summaries, x_test)
+    report(y_test, y_pred)
